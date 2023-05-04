e9e7c76534f643988995ccb4cec2fc1ff9651086
jonnalavidyasagarreddy <125711967+jonnalavidyasagarreddy@users.noreply.github.com>
2023-05-04 19:21:29 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,6 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit Load List Contains:")
 streamlit.dataframe(my_data_rows)
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Jackfruit')
+streamlit.write('The user entered ', fruit_choice)
